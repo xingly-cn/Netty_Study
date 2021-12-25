eafb852d059e2a0443b570d90dc2f675d2182b47
xingly-cn <xiaoheikeji@vip.qq.com>
2021-12-25 12:27:19 +0800

    NIO 聊天室 - 服务端

--- src/main/java/com/sugar/nio/project1/XChatServer.java
+++ src/main/java/com/sugar/nio/project1/XChatServer.java
@@ -5,7 +5,6 @@ import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.*;
 import java.util.Iterator;
-import java.util.Set;
 
 /**
  * 功能描述: X聊天室 - 服务端
@@ -14,58 +13,72 @@ import java.util.Set;
  * @date 2021/12/23 20:44
  */
 public class XChatServer {
+    // 定义属性
     private Selector selector;
     private ServerSocketChannel listenChannel;
-    private static final int PORT = 6666;
+    private static final int PORT = 6688;
 
-    public XChatServer() throws IOException {
-        selector = Selector.open();
-        listenChannel = ServerSocketChannel.open();
-        listenChannel.configureBlocking(false);
-        listenChannel.socket().bind(new InetSocketAddress(PORT));
-        listenChannel.register(selector, SelectionKey.OP_ACCEPT);
+    // 初始化
+    public XChatServer() {
+        try {
+            selector = Selector.open();
+            listenChannel = ServerSocketChannel.open();
+            listenChannel.socket().bind(new InetSocketAddress(PORT));
+            listenChannel.configureBlocking(false);
+            listenChannel.register(selector, SelectionKey.OP_ACCEPT);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
-    public void listen() throws IOException {
-        while (true) {
-            int count = selector.select();
-            if (count > 0) {
-                Iterator<SelectionKey> iterator = selector.selectedKeys().iterator();
-                while (iterator.hasNext()) {
-                    SelectionKey key = iterator.next();
-                    if (key.isAcceptable()) {
-                        SocketChannel client_channel = listenChannel.accept();
-                        client_channel.configureBlocking(false);
-                        client_channel.register(selector,SelectionKey.OP_READ);
-                        System.out.println(client_channel.getRemoteAddress() + " 上线！");
+    // 监听
+    public void listen() {
+        try {
+            while (true) {
+                int count = selector.select();
+                if (count > 0) {
+                    Iterator<SelectionKey> iterator = selector.selectedKeys().iterator();
+                    while (iterator.hasNext()) {
+                        SelectionKey key = iterator.next();
+                        if (key.isAcceptable()) {
+                            SocketChannel client_channel = listenChannel.accept();
+                            client_channel.configureBlocking(false);
+                            client_channel.register(selector,SelectionKey.OP_READ);
+                            System.out.println(client_channel.getRemoteAddress().toString().substring(1) + " 上线！");
+                        }
+                        if (key.isReadable()) {
+                            readData(key);
+                        }
+                        iterator.remove();
                     }
-                    if (key.isReadable()) {
-                        readData(key);
-                    }
-                    iterator.remove();
+                }else {
+                    System.out.println("等待中......");
                 }
-
-            }else {
-                System.out.println("等待中......");
             }
-
+        }catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            // 异常处理
         }
     }
 
     // 从客户端读取数据
     public void readData(SelectionKey key) throws IOException {
-        SocketChannel channel = (SocketChannel) key.channel();
-        ByteBuffer buffer = ByteBuffer.allocate(1024);
-        int length = channel.read(buffer);
-        if (length > 0) {
-            // 服务端接受客户端消息
-            String msg = new String(buffer.array());
-            System.out.println("From 客户端：" + msg);
-            // 消息群发给其他客户端 - 排除自己
-            sendMsgToAll(msg,channel);
-        }else {
-            // 服务端接收不到消息
-            System.out.println(channel.getRemoteAddress() + " 离线！");
+        SocketChannel channel = null;
+        try {
+            channel = (SocketChannel) key.channel();
+            ByteBuffer buffer = ByteBuffer.allocate(1024);
+            int length = channel.read(buffer);
+            if (length > 0) {
+                // 服务端接受客户端消息
+                String msg = new String(buffer.array());
+                System.out.println("From 客户端：" + msg);
+                // 消息群发给其他客户端 - 排除自己
+                 sendMsgToAll(msg,channel);
+            }
+        }catch (IOException e) {
+            System.out.println(channel.getRemoteAddress() + " 下线！");
             // 取消注册,关闭通道
             key.cancel();
             channel.close();
@@ -85,10 +98,9 @@ public class XChatServer {
                 channel.write(buffer);
             }
         }
-
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         // 启动服务端
         XChatServer xChatServer = new XChatServer();
         xChatServer.listen();
