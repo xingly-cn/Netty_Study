d59bb2182cef1e617c719729969f114ed553db79
xingly-cn <xiaoheikeji@vip.qq.com>
2021-12-25 15:29:22 +0800

    Netty入门案例 - 客户端处理器

---
+++ src/main/java/com/sugar/netty/simple/NettyClientHandler.java
@@ -0,0 +1,54 @@
+package com.sugar.netty.simple;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
+
+import java.text.CharacterIterator;
+
+/**
+ * 功能描述: Netty入门案例 - 客户端处理器
+ *
+ * @author XiaoNianXin
+ * @date 2021/12/25 15:18
+ */
+public class NettyClientHandler extends ChannelInboundHandlerAdapter {
+    /**
+     * 通道就绪时
+     * @param ctx
+     * @throws Exception
+     */
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("client :" + ctx);
+
+        // 向服务端发送数据
+        ctx.writeAndFlush(Unpooled.copiedBuffer("你好，服务器", CharsetUtil.UTF_8));
+    }
+
+    /**
+     * 异常处理
+     * @param ctx
+     * @param cause
+     * @throws Exception
+     */
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        ctx.channel().close();
+    }
+
+    /**
+     * 读取服务端数据
+     * @param ctx
+     * @param msg
+     * @throws Exception
+     */
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf buf = (ByteBuf) msg;
+        System.out.println("服务器回复：" + buf.toString(CharsetUtil.UTF_8));
+        System.out.println("服务器地址：" + ctx.channel().remoteAddress());
+    }
+}
