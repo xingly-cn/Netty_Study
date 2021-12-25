438bb28a9cd726c9da52d8c4daa2a0714348234f
xingly-cn <xiaoheikeji@vip.qq.com>
2021-12-25 16:19:03 +0800

    Netty入门案例 - 服务端控制器

--- src/main/java/com/sugar/netty/simple/NettyServerHandler.java
+++ src/main/java/com/sugar/netty/simple/NettyServerHandler.java
@@ -5,8 +5,9 @@ import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 
-import java.nio.charset.Charset;
+
 import java.nio.charset.StandardCharsets;
+import java.util.concurrent.TimeUnit;
 
 
 /**
@@ -35,7 +36,7 @@ public class NettyServerHandler extends ChannelInboundHandlerAdapter {
     @Override
     public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
         // 回复客户端消息
-        ctx.writeAndFlush(Unpooled.copiedBuffer("你好，客户端",StandardCharsets.UTF_8));
+        ctx.writeAndFlush(Unpooled.copiedBuffer("你好，客户端1",StandardCharsets.UTF_8));
     }
 
     /**
@@ -46,12 +47,42 @@ public class NettyServerHandler extends ChannelInboundHandlerAdapter {
      */
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        System.out.println("ctx = " + ctx);
+        // 如果这里有一个耗时的业务，不可能在这里等待，异步执行 ---> 任务队列
 
-        // 接受客户端消息
-        ByteBuf buf = (ByteBuf) msg;
-        System.out.println("客户端发送：" + buf.toString(StandardCharsets.UTF_8));
-        System.out.println("客户端地址：" + ctx.channel().remoteAddress());
+        // 解决方案1：用户自定义普通任务
+//        ctx.channel().eventLoop().execute(new Runnable() {
+//            @Override
+//            public void run() {
+//                try {
+//                    Thread.sleep(10 * 1000);
+//                }catch (Exception e) {
+//                    System.out.println("自定义普通任务 执行异常");
+//                }
+//                ctx.writeAndFlush(Unpooled.copiedBuffer("你好，客户端2",StandardCharsets.UTF_8));
+//            }
+//        });
+
+        // 解决方案2：用户自定义定时任务
+//        ctx.channel().eventLoop().schedule(new Runnable() {
+//            @Override
+//            public void run() {
+//                ctx.writeAndFlush(Unpooled.copiedBuffer("你好，客户端2",StandardCharsets.UTF_8));
+//            }
+//        },5, TimeUnit.SECONDS);
+
+        // 解决方案3：非 Reactor 调用 Channel 方法
+        // 略
+
+
+//        普通业务
+//        System.out.println("当前线程 = " + Thread.currentThread().getName());
+//        System.out.println("当前频道 = " + ctx.channel());      //
+//        System.out.println("当前管道 = " + ctx.pipeline());     // 底层双向链表
+//
+//        // 接受客户端消息
+//        ByteBuf buf = (ByteBuf) msg;
+//        System.out.println("客户端发送：" + buf.toString(StandardCharsets.UTF_8));
+//        System.out.println("客户端地址：" + ctx.channel().remoteAddress());
     }
 
 
