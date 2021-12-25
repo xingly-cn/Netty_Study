9d5d11d0eb9eceb824cb2ee60249ede05570eb18
xingly-cn <xiaoheikeji@vip.qq.com>
2021-12-25 12:27:08 +0800

    NIO 聊天室 - 客户端

--- src/main/java/com/sugar/nio/project1/XChatClient.java
+++ src/main/java/com/sugar/nio/project1/XChatClient.java
@@ -1,17 +1,13 @@
 package com.sugar.nio.project1;
 
-import com.sun.scenario.effect.impl.prism.PrImage;
-
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
-import java.nio.channels.SelectableChannel;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.SocketChannel;
 import java.util.Iterator;
 import java.util.Scanner;
-import java.util.Set;
 
 /**
  * 功能描述: X聊天室 - 客户端
@@ -21,7 +17,7 @@ import java.util.Set;
  */
 public class XChatClient {
     private final String HOST = "127.0.0.1";
-    private final int PORT = 6666;
+    private final int PORT = 6688;
     private Selector selector;
     private SocketChannel socketChannel;
     private String UserName;
@@ -31,33 +27,41 @@ public class XChatClient {
         socketChannel = SocketChannel.open(new InetSocketAddress(HOST,PORT));
         socketChannel.configureBlocking(false);
         socketChannel.register(selector, SelectionKey.OP_READ);
-        UserName = socketChannel.getLocalAddress().toString();
+        UserName = socketChannel.getLocalAddress().toString().substring(1);
         System.out.println(UserName + " 成功连接服务器...");
     }
 
-    public void sendMsg(String msg) throws IOException {
+    public void sendMsg(String msg)  {
         msg = UserName + "：" + msg;
-        ByteBuffer buffer = ByteBuffer.wrap(msg.getBytes());
-        socketChannel.write(buffer);
+        try {
+            socketChannel.write(ByteBuffer.wrap(msg.getBytes()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     // 客户端也需监听服务端发来的消息 - 使用 select
-    public void readMsg() throws IOException {
-        int read = selector.select();
-        if (read > 0) {
-            Iterator<SelectionKey> iterator = selector.selectedKeys().iterator();
-            while (iterator.hasNext()) {
-                SelectionKey key = iterator.next();
-                if (key.isReadable()) {
-                    SocketChannel channel = (SocketChannel) key.channel();
-                    ByteBuffer buffer = ByteBuffer.allocate(1024);
-                    channel.read(buffer);
-                    String msg = new String(buffer.array());
-                    System.out.println(msg.trim());
+    public void readMsg()  {
+        try {
+            int read = selector.select();
+            if (read > 0) {
+                Iterator<SelectionKey> iterator = selector.selectedKeys().iterator();
+                while (iterator.hasNext()) {
+                    SelectionKey key = iterator.next();
+                    if (key.isReadable()) {
+                        SocketChannel channel = (SocketChannel) key.channel();
+                        ByteBuffer buffer = ByteBuffer.allocate(1024);
+                        channel.read(buffer);
+                        String msg = new String(buffer.array());
+                        System.out.println(msg.trim());
+                    }
                 }
+                iterator.remove();
+            }else {
+                // System.out.println("没收到消息......");
             }
-        }else {
-            System.out.println("没收到消息......");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
@@ -70,10 +74,10 @@ public class XChatClient {
         new Thread() {
             public void run() {
                 while (true) {
+                    xChatClient.readMsg();
                     try {
-                        xChatClient.readMsg();
                         Thread.currentThread().sleep(3000);
-                    } catch (IOException | InterruptedException e) {
+                    } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
                 }
